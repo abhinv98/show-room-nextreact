8c8815e85b68277fefd1b26004e4f1606e4f4d6a
abhiCS7A60 <88727740+abhinv98@users.noreply.github.com>
2023-09-16 09:49:26 +0530

    hero and footer

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,15 +1,15 @@
 "use client";
 import { CustomButtonProps } from "@/types";
 import Image from "next/image";
-const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) => {
+const CustomButton = ({title, containerStyles, btnType, handleClick}: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type={"button"}
-      className={`custom-btn`}
-      onClick={() => {}}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
     >
-        <span className={`flex-1`}>title</span>
+        <span className={`flex-1`}>{title}</span>
     </button>
   );
 };
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,29 +1,33 @@
-'use client';
+"use client";
 
-import Image from 'next/image'
-import { CustomButton } from '.'
+import Image from "next/image";
+import { CustomButton } from ".";
 
 const Hero = () => {
-  const handleScroll = () => {
-
-  }
+  const handleScroll = () => {};
   return (
-    <div className='hero'>
-        <div className='flex-1 pt-36 padding-x'>
-            <h1 className='hero__title'>
-                Search for the cars you love and go crazy.
-            </h1>
-            <p className='hero__subtitle'>
-                Your car rental experience with out effortless booking process.
-            </p>
-            <CustomButton 
-            title = "Explore Cars"
-            containerStyles = "bg-primary-blue text-white rounded-full mt-10"
-            handleClick = {handleScroll}
-            />
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Search for the cars you love and go crazy.
+        </h1>
+        <p className="hero__subtitle">
+          Your car rental experience with our effortless booking process.
+        </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+        <div className="hero__image-overlay" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
