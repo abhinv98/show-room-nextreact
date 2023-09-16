663f0d2e01bd8eca4f0f48b977acc7af21fcdd65
abhiCS7A60 <88727740+abhinv98@users.noreply.github.com>
2023-09-16 12:22:37 +0530

    search dialog box

--- types/index.ts
+++ types/index.ts
@@ -7,3 +7,8 @@ export interface CustomButtonProps{
     MouseEventHandler<HTMLButtonElement> 
     btnType?: "button" | "submit";
 }
+
+export interface SearchManufacturerProps{
+    manufacturer: string;
+    setmanufacturer: (manufacturer: string) => void;
+}
